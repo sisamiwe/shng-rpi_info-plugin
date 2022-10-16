45ba7f288de0c0b9be7a09e96a9c399ea1cb4d66
Michael <wenzel_michael@web.de>
2022-10-16 10:40:04 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#########################################################################
+#  Copyright 2020-      <AUTHOR>                                  <EMAIL>
+#########################################################################
+#  This file is part of SmartHomeNG.
+#  https://www.smarthomeNG.de
+#  https://knx-user-forum.de/forum/supportforen/smarthome-py
+#
+#  Sample plugin for new plugins to run with SmartHomeNG version 1.8 and
+#  upwards.
+#
+#  SmartHomeNG is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHomeNG is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
+
+from lib.model.smartplugin import SmartPlugin
+from lib.item import Items
+
+from .webif import WebInterface
+
+
+# If a needed package is imported, which might be not installed in the Python environment,
+# add it to a requirements.txt file within the plugin's directory
+
+
+class SamplePlugin(SmartPlugin):
+    """
+    Main class of the Plugin. Does all plugin specific stuff and provides
+    the update functions for the items
+
+    HINT: Please have a look at the SmartPlugin class to see which
+    class properties and methods (class variables and class functions)
+    are already available!
+    """
+
+    PLUGIN_VERSION = '1.0.0'    # (must match the version specified in plugin.yaml), use '1.0.0' for your initial plugin Release
+
+    def __init__(self, sh):
+        """
+        Initalizes the plugin.
+
+        If you need the sh object at all, use the method self.get_sh() to get it. There should be almost no need for
+        a reference to the sh object any more.
+
+        Plugins have to use the new way of getting parameter values:
+        use the SmartPlugin method get_parameter_value(parameter_name). Anywhere within the Plugin you can get
+        the configured (and checked) value for a parameter by calling self.get_parameter_value(parameter_name). It
+        returns the value in the datatype that is defined in the metadata.
+        """
+
+        # Call init code of parent class (SmartPlugin)
+        super().__init__()
+
+        # get the parameters for the plugin (as defined in metadata plugin.yaml):
+        try:
+            # webif_pagelength should be included in all plugins using a web interface
+            # It is used to overwrite the default max number of entries per page in the tables
+            self.webif_pagelength = self.get_parameter_value('webif_pagelength')
+            # self.param1 = self.get_parameter_value('param1')
+        except KeyError as e:
+            self.logger.critical("Plugin '{}': Inconsistent plugin (invalid metadata definition: {} not defined)".format(self.get_shortname(), e))
+            self._init_complete = False
+            return
+
+        # cycle time in seconds, only needed, if hardware/interface needs to be
+        # polled for value changes by adding a scheduler entry in the run method of this plugin
+        # (maybe you want to make it a plugin parameter?)
+        self._cycle = 60
+
+        # Initialization code goes here
+
+        # On initialization error use:
+        #   self._init_complete = False
+        #   return
+
+        # if plugin should start even without web interface
+        self.init_webinterface(WebInterface)
+        # if plugin should not start without web interface
+        # if not self.init_webinterface():
+        #     self._init_complete = False
+
+        return
+
+    def run(self):
+        """
+        Run method for the plugin
+        """
+        self.logger.debug("Run method called")
+        # setup scheduler for device poll loop   (disable the following line, if you don't need to poll the device. Rember to comment the self_cycle statement in __init__ as well)
+        self.scheduler_add('poll_device', self.poll_device, cycle=self._cycle)
+
+        self.alive = True
+        # if you need to create child threads, do not make them daemon = True!
+        # They will not shutdown properly. (It's a python bug)
+
+    def stop(self):
+        """
+        Stop method for the plugin
+        """
+        self.logger.debug("Stop method called")
+        self.scheduler_remove('poll_device')
+        self.alive = False
+
+    def parse_item(self, item):
+        """
+        Default plugin parse_item method. Is called when the plugin is initialized.
+        The plugin can, corresponding to its attribute keywords, decide what to do with
+        the item in future, like adding it to an internal array for future reference
+        :param item:    The item to process.
+        :return:        If the plugin needs to be informed of an items change you should return a call back function
+                        like the function update_item down below. An example when this is needed is the knx plugin
+                        where parse_item returns the update_item function when the attribute knx_send is found.
+                        This means that when the items value is about to be updated, the call back function is called
+                        with the item, caller, source and dest as arguments and in case of the knx plugin the value
+                        can be sent to the knx with a knx write function within the knx plugin.
+        """
+        if self.has_iattr(item.conf, 'foo_itemtag'):
+            self.logger.debug(f"parse item: {item}")
+
+        # todo
+        # if interesting item for sending values:
+        #   self._itemlist.append(item)
+        #   return self.update_item
+
+    def parse_logic(self, logic):
+        """
+        Default plugin parse_logic method
+        """
+        if 'xxx' in logic.conf:
+            # self.function(logic['name'])
+            pass
+
+    def update_item(self, item, caller=None, source=None, dest=None):
+        """
+        Item has been updated
+
+        This method is called, if the value of an item has been updated by SmartHomeNG.
+        It should write the changed value out to the device (hardware/interface) that
+        is managed by this plugin.
+
+        :param item: item to be updated towards the plugin
+        :param caller: if given it represents the callers name
+        :param source: if given it represents the source
+        :param dest: if given it represents the dest
+        """
+        if self.alive and caller != self.get_shortname():
+            # code to execute if the plugin is not stopped
+            # and only, if the item has not been changed by this this plugin:
+            self.logger.info(f"Update item: {item.property.path}, item has been changed outside this plugin")
+
+            if self.has_iattr(item.conf, 'foo_itemtag'):
+                self.logger.debug(f"update_item was called with item {item.property.path} from caller {caller}, source {source} and dest {dest}")
+            pass
+
+    def poll_device(self):
+        """
+        Polls for updates of the device
+
+        This method is only needed, if the device (hardware/interface) does not propagate
+        changes on it's own, but has to be polled to get the actual status.
+        It is called by the scheduler which is set within run() method.
+        """
+        # # get the value from the device
+        # device_value = ...
+        #
+        # # find the item(s) to update:
+        # for item in self.sh.find_items('...'):
+        #
+        #     # update the item by calling item(value, caller, source=None, dest=None)
+        #     # - value and caller must be specified, source and dest are optional
+        #     #
+        #     # The simple case:
+        #     item(device_value, self.get_shortname())
+        #     # if the plugin is a gateway plugin which may receive updates from several external sources,
+        #     # the source should be included when updating the the value:
+        #     item(device_value, self.get_shortname(), source=device_source_id)
+        pass
